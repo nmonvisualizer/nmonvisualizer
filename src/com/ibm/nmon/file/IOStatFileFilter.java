3fb013ebe9c84de3702435074ed51c2ccc077f0d
Hunter Presnall <presnall@us.ibm.com>
2013-07-30 09:46:11 -0400

    Bug fix for archives matching NMON and IOStat files

--- src/com/ibm/nmon/file/IOStatFileFilter.java
+++ src/com/ibm/nmon/file/IOStatFileFilter.java
@@ -3,7 +3,20 @@ package com.ibm.nmon.file;
 public final class IOStatFileFilter extends BaseFileFilter {
     public boolean accept(String pathname) {
         String name = pathname.toLowerCase();
-        return name.contains("iostat");
+
+        if (name.endsWith(".iostat")) {
+            return true;
+        }
+        else {
+            int idx = name.lastIndexOf('/');
+
+            if (idx != -1) {
+                name = pathname.substring(idx + 1);
+            }
+
+            return name.contains("iostat")
+                    && !(name.endsWith(".zip") || name.endsWith(".gz") || name.endsWith(".tar") || name.endsWith(".7z"));
+        }
     }
 
     IOStatFileFilter() {}
--- src/com/ibm/nmon/file/NMONFileFilter.java
+++ src/com/ibm/nmon/file/NMONFileFilter.java
@@ -2,18 +2,20 @@ package com.ibm.nmon.file;
 
 public final class NMONFileFilter extends BaseFileFilter {
     public boolean accept(String pathname) {
-        if (pathname.endsWith(".nmon")) {
+        String name = pathname.toLowerCase();
+
+        if (name.endsWith(".nmon")) {
             return true;
         }
         else {
-            int idx = pathname.lastIndexOf('/');
+            int idx = name.lastIndexOf('/');
 
-            if (idx == -1) {
-                return pathname.contains("nmon");
+            if (idx != -1) {
+                name = pathname.substring(idx + 1);
             }
-            else {
-                return pathname.substring(idx + 1).contains("nmon");
-            }
+
+            return name.contains("nmon")
+                    && !(name.endsWith(".zip") || name.endsWith(".gz") || name.endsWith(".tar") || name.endsWith(".7z"));
         }
     }
 
